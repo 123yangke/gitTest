fb252b6c6c401061f6508501486c212e272c8e7d
yangke <2326914884@qq.com>
2021-11-28 19:32:12 +0800

    model commit1

---
+++ model1/src/main/java/com/hulike/git/GitTestModel1.java
@@ -0,0 +1,11 @@
+package com.hulike.git;
+
+/**
+ * @author yang'ke
+ * @create 2021-11-28 19:31
+ */
+public class GitTestModel1 {
+    public static void main(String[] args) {
+        System.out.println("git1");
+    }
+}
