a66a4cfa1ad1893332f43241bf3b3d817cc4971f
yangke <2326914884@qq.com>
2021-11-28 18:41:24 +0800

    update .gitignore

---
+++ src/main/java/com/hulike/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.hulike.git;
+
+/**
+ * @author yang'ke
+ * @create 2021-11-28 18:38
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git1");
+    }
+}
